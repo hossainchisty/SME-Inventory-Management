8a5895933beb6bca3cd99f79959735ae478f7dd3
hossainchisty <hossain.chisty11@gmail.com>
2023-01-12 00:15:08 +0600

    ✨ Added templatetags

---
+++ supplier/templatetags/total_supplier.py
@@ -0,0 +1,13 @@
+from django import template
+
+from supplier.models import Supplier
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_total_supplier(defalut=0):
+    '''
+    This method is used to calculate the total supplier.
+    '''
+    return Supplier.objects.all().count() or defalut
