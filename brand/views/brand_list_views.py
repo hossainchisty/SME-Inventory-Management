0c97f56c569551fe495bd9f493a612d95b197256
hossainchisty <hossain.chisty11@gmail.com>
2023-01-12 15:09:28 +0600

    🚑 Fix paginator number

--- brand/views/brand_list_views.py
+++ brand/views/brand_list_views.py
@@ -10,7 +10,7 @@ class BrandListView(View):
     def get(self, request):
         ''' This will reutrn list of brand '''
         brand_list = Brand.objects.all().order_by('-id')
-        paginator = Paginator(brand_list, 4)
+        paginator = Paginator(brand_list, 10)
         page_number = request.GET.get('page')
 
         try:
