cafc0ded6a5c7ee8e6e1904ea3a3b67587167b55
hossainchisty <hossain.chisty11@gmail.com>
2023-01-12 15:31:53 +0600

    🔥 Remove brand and category listing

--- product/views/product_views.py
+++ product/views/product_views.py
@@ -1,48 +1,26 @@
+# Basic Lib Import
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 from django.views.generic import View
 
-from product.models import Brand, Category, Product
+from product.models import Product
 
 
 class ProductListView(View):
     def get(self, request):
         ''' This will reutrn list of products '''
         product_list = Product.objects.all().order_by('-id')
-
-        paginator = Paginator(product_list, 25)
+        paginator = Paginator(product_list, 5)
         page_number = request.GET.get('page')
-        products = paginator.get_page(page_number)
-
+        try:
+            page_object = paginator.page(page_number)
+        except PageNotAnInteger:
+            # if page is not an integer, deliver the first page
+            page_object = paginator.page(1)
+        except EmptyPage:
+            # if the page is out of range, deliver the last page
+            page_object = paginator.page(paginator.num_pages)
         context = {
-            'products': products,
+            'products': page_object,
         }
         return render(request, 'product/product_list.html', context)
-
-
-class CategoryListView(View):
-    def get(self, request):
-        ''' This will reutrn list of category '''
-        category_list = Category.objects.all().order_by('-id')
-        paginator = Paginator(category_list, 25)
-        page_number = request.GET.get('page')
-        category = paginator.get_page(page_number)
-
-        context = {
-            'category': category,
-        }
-        return render(request, 'product/category_list.html', context)
-
-
-class BrandListView(View):
-    def get(self, request):
-        ''' This will reutrn list of brand '''
-        brand_list = Brand.objects.all().order_by('-id')
-        paginator = Paginator(brand_list, 25)
-        page_number = request.GET.get('page')
-        brand = paginator.get_page(page_number)
-
-        context = {
-            'brand': brand,
-        }
-        return render(request, 'product/brand_list.html', context)
