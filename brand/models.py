bde0c608861bc98bcd6a9a571a856806d4f6fff5
hossainchisty <hossain.chisty11@gmail.com>
2023-01-12 15:07:27 +0600

    ✨ Added new field

--- brand/models.py
+++ brand/models.py
@@ -5,6 +5,7 @@ from utility.common_fields import BaseModel
 
 class Brand(BaseModel):
     name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
