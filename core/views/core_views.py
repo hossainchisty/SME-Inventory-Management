43ed7e630e876d3477d2517b5c2bf59fa3445ccb
hossainchisty <hossain.chisty11@gmail.com>
2023-04-24 14:45:48 +0600

    ✨ Update core view

--- core/views/core_views.py
+++ core/views/core_views.py
@@ -4,4 +4,4 @@ from django.views.generic import View
 class Dashboard(View):
 
     def get(self, request):
-        return render(request, 'core/dashboard.html', context)
+        return render(request, 'core/dashboard.html')
