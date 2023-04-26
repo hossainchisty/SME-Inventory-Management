4e6f1f9f0b06b19b98c1e0501fcbf7d8fb115387
hossainchisty <hossain.chisty11@gmail.com>
2023-04-26 23:37:03 +0600

    🚀 Update build scripts

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
+python -m pip install --upgrade pip
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
