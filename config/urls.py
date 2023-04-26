b4b72ee97fdda0ef52602e1b66b98133ab3ad6d8
hossainchisty <hossain.chisty11@gmail.com>
2023-04-27 00:01:42 +0600

    ✨ Added custome commends

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+# from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view
+# from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,23 +23,23 @@ urlpatterns += [
 ]
 
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="SME Inventory Management API",
-      default_version='v1',
-      description="Inventory management refers to the process of ordering, storing, using, and selling a company's inventory. This includes the management of raw materials, components, and finished products and processing of such items.",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="hossain.chisty11@gmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="SME Inventory Management API",
+#       default_version='v1',
+#       description="Inventory management refers to the process of ordering, storing, using, and selling a company's inventory. This includes the management of raw materials, components, and finished products and processing of such items.",
+#       terms_of_service="https://www.google.com/policies/terms/",
+#       contact=openapi.Contact(email="hossain.chisty11@gmail.com"),
+#       license=openapi.License(name="BSD License"),
+#    ),
+#    public=True,
+#    permission_classes=[permissions.AllowAny],
+# )
 
 urlpatterns += [
-   path('swagger/json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   # path('swagger/json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Admin panel configure
---
+++ core/management/commands/createsuperman.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(email='admin@gmail.com').exists():
+            User.objects.create_superuser(
+                email='admin',
+                first_name='Super',
+                last_name='Man',
+                phone_number='01222927392',
+                password='complexpassword123'
+            )
+        print('Superuser has been created.')
